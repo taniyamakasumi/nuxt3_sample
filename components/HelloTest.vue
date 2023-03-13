19b59f404a4ca9d68ea745f423e0ef2510d8c1f6
Kasumi Taniyama <kasumi.taniyama@gmail.com>
2023-03-13 21:04:39 +0900

    初期化

---
+++ components/HelloTest.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>hello test2</div>
+    <ul>
+        <li v-for="(item, index) in testArr" :key="index"> 
+        <!-- in 要素の数 -->
+        {{item}}
+        <span v-if="`${item}`=== 'C'">これはCです</span>
+        </li>
+        <div>{{add}}</div>
+    </ul>
+    <!-- <div v-text="text"></div> -->
+        <!-- <div v-text="`${text}`"></div> -->
+</template>
+<script>
+export default {
+    data(){
+        return{
+            testArr:  ["A","B","C"],
+            text: "あ",
+            num1: 1,
+            num2: 3
+        }
+    },
+    computed:{
+        add(){
+            return this.num1 + this.num2
+            // computedでdataから変数を使う時はthiss
+        }
+    }
+}
+</script>
---
+++ pages/hello.vue
@@ -0,0 +1,4 @@
+<template>
+    <div>hello</div>
+    <HelloTest />
+</template>
